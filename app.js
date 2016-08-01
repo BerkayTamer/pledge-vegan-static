97f81b55cea9b7e4b8e202756d654688b3356adf
Berkay Tamer <berkay_tamer1907@hotmail.com>
2016-08-01 09:17:12 -0600

    Refactored app.js

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 var request = require('request');
 var fs = require('fs');
 var Twig = require('twig');
+var async = require('async');
 var twig = Twig.twig;
 
-
-fs.readFile('quotes.twig', 'utf8', function(err, contents) {
-
-    var template = twig({
-        data: contents
-    });
-
-
-    request('https://berkaytamer.github.io/top-vegan-quotes/api.json', function (error, response, data) {
-        if (!error && response.statusCode == 200) {
-
-            templateData = {};
-            templateData.quotes = JSON.parse(data);
-
-            var renderedTemplate = template.render(templateData);
-
-            fs.writeFile('index.html', renderedTemplate, function (err) {
-                if (err) throw err;
-
+async.waterfall([
+    function (callback) {
+        fs.readFile('quotes.twig', 'utf8', function (err, contents) {
+            var template = twig({
+                data: contents
             });
-
-        }
-    });
-
+            console.log(template)
+            callback(err, template);
+        });
+    },
+    function (template, callback) {
+        request('https://berkaytamer.github.io/top-vegan-quotes/api.json', function (err, res) {
+            if (err) {
+                callback(err);
+            }
+            templateData =  JSON.parse(res.body);
+            var renderedTemplate = template.render(templateData);
+            callback(null, renderedTemplate);
+        });
+    },
+    function (renderedTemplate, callback) {
+        fs.writeFile('index.html', renderedTemplate, function (err, data) {
+            if (err) {
+                callback(err);
+            }
+            callback(null, data);
+        });
+    }
+], function (err, data) {
+    if (err) {
+        process.stderr.write(err);
+        process.exit(0);
+    }
+    console.log('datas written', data)
 });
